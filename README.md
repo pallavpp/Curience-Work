b18e388f7b87d8d1cdad3638f7047696e3251026
pallavpp <pallav.pandey2011@gmail.com>
2021-12-24 01:54:32 +0530

    added script to filter and combine data based on keywords, updated readme

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Curience-Work
-Pallav and Alokeveer's Work
+Pallav and Alokeveer's Work.
 
 ## Info
 - Site specific scraping scripts present in `Scripts` directory.
 - Custom modules are present in `Modules` directory.
 - Scraped data is saved in CSV format in `Data` directory. Scripts automatically save CSV in Data folder.
-- For scripts usign selenium, user will have to update driver path in script before executing.
 - Add following code to scripts to import custom modules:
 ```
 import os
@@ -14,6 +13,10 @@ modules_path  = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Modu
 if modules_path not in sys.path:
     sys.path.insert(1, modules_path)
 ```
+- Before running scripts that use selenium, user will have to update driver path in script.
+- `Scripts/final_data.py` will filter all CSV files present in `Data` based on keywords, and store the combined data in a CSV file inside `Data`.
+    - ***Keyword list present inside `Scripts/final_data.py` has to be manually updated.***
+    - For all scraping scripts, use the following column names only: [“Blog Title”, “Blog Date”, “Blog Link”, “Author Name”, “Author Profile Link”, “Thumbnail Link”, “Thumbnail Credit”]. ***If you decide to include any new column name not listed here, add it to the column list present inside `Scripts/final_data.py` and update in the above list as well. If not done, the new column will not be used while filtered data.***
 
 ## All Requirements
 - [Selenium](https://pypi.org/project/selenium/)
---
+++ Scripts/final_data.py
@@ -0,0 +1,50 @@
+# standard imports
+import glob
+import pandas as pd
+
+# custom imports
+import os
+import sys
+modules_path  = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Modules"))
+if modules_path not in sys.path:
+    sys.path.insert(1, modules_path)
+import SaveDataAsCSV
+
+# to be updated manually
+keywords = ["spring", "2022"]
+column_names = ["Blog Title", "Blog Date", "Blog Link", "Author Name", "Author Profile Link", "Thumbnail Link", "Thumbnail Credit"]
+
+# function to find if any keyword is present in blog title
+def filter_using_keywords(blog_title):
+    flag = False
+    for keyword in keywords:
+        if keyword in blog_title:
+            flag = True
+            break
+    return flag
+
+# main process
+def extract():
+    final_df = pd.DataFrame(columns=column_names)
+    data_folder_path  = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Data"))
+
+    for path in glob.glob(data_folder_path + "\*.csv"):
+        if os.path.basename(path) != "final_data.csv":
+            curr_df = pd.read_csv(path)
+            row_bool = curr_df.apply(lambda x: filter_using_keywords(x["Blog Title"].lower()), axis=1)
+            curr_df = curr_df[row_bool]
+            final_df = pd.concat([final_df, curr_df], ignore_index=True)
+
+    # removing duplicate blog links
+    final_df.drop_duplicates(subset=["Blog Link"], inplace=True, ignore_index=True)
+
+    # replacing NaN with empty string
+    final_df.fillna("", inplace=True)
+
+    # saving combined data as csv
+    SaveDataAsCSV.df_to_csv_in_data(dataframe=final_df, caller_path=__file__)
+
+if __name__ == "__main__":
+	print("Processing...")
+	extract()
+	print("Finished.")
